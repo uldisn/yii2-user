c6c288700521a5b5809a67e0def1d063fc0a8d83
Uldis Nelsons <uldisnelsons@gmail.com>
2013-11-15 22:14:25 +0200

    created latvian translation

---
+++ messages/lv/UWrelBelongsTo.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(
+	'Model Name'=>'Modeļa nosaukums',
+	'Lable field name'=>'Lauka nosaukums',
+	'Empty item name'=>'Tukša lauka nosaukums',
+	'Profile model relation name'=>'Modeļa profila relācijas nosaukums',
+);

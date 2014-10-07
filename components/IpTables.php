56426f923dc6c4276b2e8d5477cdd9c5b1b0d6b7
ivarsju <ivars.ju@gmail.com>
2014-10-07 03:15:17 +0300

    Better ip2long convertation

--- components/IpTables.php
+++ components/IpTables.php
@@ -2,6 +2,7 @@
 
 class IpTables
 {
+    
     public static function validate($user_id)
     {
         
@@ -9,13 +10,11 @@ class IpTables
             return false;
         }
         
-        $long_ip = ip2long($_SERVER['REMOTE_ADDR']);
+        $long_ip = self::_ip2long($_SERVER['REMOTE_ADDR']);
         
-        if ($long_ip == -1 || $long_ip === false) {
+        if ($long_ip === false) {
             return false;
         }
-        
-        $long_ip = sprintf('%u', $long_ip);
         
         $criteria = new CDbCriteria();
         $criteria->compare('t.uxip_user_id', $user_id);
@@ -27,19 +26,12 @@ class IpTables
             
             if ($ip->iptb_status == BaseIptbIpTable::IPTB_STATUS_ACTIVE) {
                 
-                $long_from = ip2long($ip->iptb_from);
-                $long_to   = ip2long($ip->iptb_to);
+                $long_from = self::_ip2long($ip->iptb_from);
+                $long_to   = self::_ip2long($ip->iptb_to);
                 
-                if ($long_from == -1 || $long_from === false) {
+                if ($long_from === false || $long_to === false) {
                     continue;
                 }
-                
-                if ($long_to == -1 || $long_to === false) {
-                    continue;
-                }
-                
-                $long_from = sprintf('%u', $long_from);
-                $long_to   = sprintf('%u', $long_to);
                 
                 if ($long_ip >= $long_from && $long_ip <= $long_to) {
                     return true;
@@ -52,5 +44,25 @@ class IpTables
         return false;
         
     }
+    
+    private static function _ip2long($ip)
+    {
+        
+        $pattern = '#^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$#';
+        
+        if (!preg_match($pattern, $ip)) {
+            return false;
+        }
+        
+        if (substr($ip, 0, 3) > 127) {
+            $long_ip = ((ip2long($ip) & 0x7FFFFFFF) + 0x80000000);
+        } else {
+            $long_ip = ip2long($ip);
+        }
+        
+        return $long_ip;
+        
+    }
+    
 }
 
